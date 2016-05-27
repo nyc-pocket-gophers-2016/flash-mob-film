ddb18216afb8a9611c1a5337044db3b32f9d56ea
Calvin & parker smathers & Chand <pair+icalving.psmathers.radicaldesi@devbootcamp.com>
2016-05-27 12:30:30 -0400

    Fix error

--- db/migrate/20160527120911_create_cards.rb
+++ db/migrate/20160527120911_create_cards.rb
@@ -2,7 +2,7 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.string :question, null: false
-      t.string :answer, null: false
+      t.string :answer
       t.integer :deck_id, null: false
 
       t.timestamps(null: false)
