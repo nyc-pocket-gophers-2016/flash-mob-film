3f7179f7b180bf591082322206d7dc99d3615bd6
Calvin & parker smathers & Chand <pair+icalving.psmathers.radicaldesi@devbootcamp.com>
2016-05-27 12:32:37 -0400

    Fix error

--- db/migrate/20160527121134_create_guesses.rb
+++ db/migrate/20160527121134_create_guesses.rb
@@ -1,8 +1,8 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
-      t.string :guess_response, null: false
-      t.boolean :correct_answer?, null: false
+      t.string :guess_response
+      t.boolean :correct_answer?
       t.integer :card_id, null: false
       t.integer :user_id, null: false
       t.integer :round_id, null: false
